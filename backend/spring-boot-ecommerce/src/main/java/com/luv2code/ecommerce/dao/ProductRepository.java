ba7eae7aef0f5fc2aa71f46b3e234f83bbdc42cb
SujayTank <61505947+SujayTank@users.noreply.github.com>
2021-08-30 14:46:22 +0530

    configure only for read only

--- backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -4,5 +4,4 @@ import com.luv2code.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
 }
